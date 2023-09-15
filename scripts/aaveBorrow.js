e5e6f6e469d1333bcb0122fa2e9280c5d3210ef5
Trung Trần <trungtran@MacBook-Air-cua-Trung.local>
2023-09-15 21:10:31 +0700

    create LendingPool contract instance

--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -1,7 +1,35 @@
+const { ethers, getNamedAccounts } = require("hardhat");
 const { getWeth } = require("./getWeth");
 
 async function main() {
-    await getWeth();
+    // await getWeth();
+
+    const { deployer } = await getNamedAccounts();
+    // Lending Pool Address Provider: 0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5
+    // We can get the Lending Pool address base on this contract, then we will create a Lending Pool contract instance
+    const lendingPool = await getLendingPoolContract(deployer);
+    console.log(`Lending pool address: ${lendingPool.address}`);
+}
+
+async function getLendingPoolContract(deployer) {
+    // Let's create a Lending Pool Address Provider contract instance to call getLendingPool()
+    // abi + address
+
+    const ILendingPoolAddressesProvider = await ethers.getContractAt(
+        "contracts/interfaces/ILendingPoolAddressesProvider.sol:ILendingPoolAddressesProvider",
+        "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5",
+        deployer
+    );
+    const lendingPoolAddress =
+        await ILendingPoolAddressesProvider.getLendingPool();
+
+    const ILendingPool = await ethers.getContractAt(
+        "ILendingPool",
+        lendingPoolAddress,
+        deployer
+    );
+
+    return ILendingPool;
 }
 
 main()
--- scripts/getWeth.js
+++ scripts/getWeth.js
@@ -7,16 +7,16 @@ async function getWeth() {
 
     // we will interact with WETH contract on mainnet now
     // To get contract's instance, we need: abi + address
-    const iWethContract = await ethers.getContractAt(
+    const iWeth = await ethers.getContractAt(
         "IWeth", // this is interface, can use instead of abi
         "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", // this is WETH contract address on ETH mainnet
         deployer
     );
-    const txResponse = await iWethContract.deposit({
+    const txResponse = await iWeth.deposit({
         value: AMOUNT,
     });
     await txResponse.wait(1);
-    const wethBalance = await iWethContract.balanceOf(deployer);
+    const wethBalance = await iWeth.balanceOf(deployer);
     console.log(`Got ${ethers.utils.formatEther(wethBalance)} WETH`);
 }
 
